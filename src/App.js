ba997138162bf6243ec56250e4b708d786cfc1bf
Seu nome <senamatheus.job@gmail.com>
2022-01-27 15:38:58 -0300

    adicionando pages e rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import Explore from './pages/Explore';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/foods" exact component={ Foods } />
+        <Route path="/drinks" exact component={ Drinks } />
+        <Route path="/explore" exact component={ Explore } />
+        <Route path="/donerecipes" exact component={ DoneRecipes } />
+        <Route path="/favoriterecipes" exact component={ FavoriteRecipes } />
+        <Route path="/profile" exact component={ Profile } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
