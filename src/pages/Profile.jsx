73ef6a12b4f0c6a65292473eb374f9f6916c1d4e
Seu nome <senamatheus.job@gmail.com>
2022-01-28 07:12:31 -0300

    profile page done. Req 82-87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const history = useHistory();
+
+  const toFavoritesRecipes = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const toDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const logOutFunction = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
   return (
     <div>
       <Header title="Profile" search="" />
-      <h1>Olá Profile</h1>
+      <h1 data-testid="profile-email">{ user.email }</h1>
+      <section>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ toDoneRecipes }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ toFavoritesRecipes }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          onClick={ logOutFunction }
+          data-testid="profile-logout-btn"
+          type="button"
+        >
+          Logout
+        </button>
+      </section>
       <Footer />
     </div>
   );
