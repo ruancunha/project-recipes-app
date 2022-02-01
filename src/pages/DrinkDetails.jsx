c41416dd827a637599d2088a43c2795eef68f08b
Seu nome <senamatheus.job@gmail.com>
2022-02-01 09:00:03 -0300

    req 36 e 42

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -1,25 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { drinksDetailsFetch } from '../services';
+import { drinksDetailsFetch, mealsNameFetch } from '../services';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeart from '../images/whiteHeartIcon.svg';
 
 function DrinkDetails(props) {
   const [recipe, setRecipe] = useState('');
+  const [reco, setReco] = useState('');
 
   const getRecipe = async () => {
+    const magicNumber = 6;
     const { match } = props;
     const { id } = match.params;
     const result = await drinksDetailsFetch(id);
-    setRecipe(result);
+    const recomendation = await mealsNameFetch('');
+    setRecipe(result[0]);
+    setReco(recomendation.slice(0, magicNumber));
   };
 
   useEffect(() => {
     getRecipe();
   }, []);
 
-  console.log(recipe);
+  const { strDrink, strDrinkThumb,
+    strInstructions, strCategory, strAlcoholic } = recipe;
+  console.log(reco);
+
   return (
     <div>
-      oi
+      <img data-testid="recipe-photo" src={ strDrinkThumb } alt="food" />
+      <h2 data-testid="recipe-title">{strDrink}</h2>
+      <h4>{strAlcoholic}</h4>
+      <button data-testid="share-btn" type="button">
+        <img src={ shareIcon } alt="share" />
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        <img src={ whiteHeart } alt="favorite" />
+      </button>
+      <h4 data-testid="recipe-category">{strCategory}</h4>
+      <p data-testid="instructions">{strInstructions}</p>
+      <section>
+        <h1>recomendation</h1>
+        {/* {
+          reco.map((rec, index) => (
+            <div key={ `data-testid="${index}-recomendation-card"` }>
+              <img src={ rec.strMealThumb } alt="recomendation" />
+              <h3>{rec.strCategory}</h3>
+              <h3>{rec.strMeal}</h3>
+            </div>
+          ))
+        } */}
+      </section>
+      <button data-testid="start-recipe-btn" type="button">
+        Start Recipe
+      </button>
     </div>
   );
 }
--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,25 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { foodDetailsFetch } from '../services';
+import { cocktailsNameFetch, foodDetailsFetch } from '../services';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeart from '../images/whiteHeartIcon.svg';
 
 function FoodDetails(props) {
   const [recipe, setRecipe] = useState('');
+  const [reco, setReco] = useState('');
 
   const getRecipe = async () => {
+    const magicNumber = 6;
     const { match } = props;
     const { id } = match.params;
     const result = await foodDetailsFetch(id);
-    setRecipe(result);
+    const recomendation = await cocktailsNameFetch('');
+    setRecipe(result[0]);
+    setReco(recomendation.slice(0, magicNumber));
   };
+
+  console.log(reco);
 
   useEffect(() => {
     getRecipe();
   }, []);
 
-  console.log(recipe);
+  const { strMeal, strMealThumb, strCategory, strInstructions } = recipe;
+
+  // data-testid="${index}-ingredient-name-and-measure";
+  // data-testid="video";
+  // d;
+
   return (
     <div>
-      oi
+      <img data-testid="recipe-photo" src={ strMealThumb } alt="food" />
+      <h2 data-testid="recipe-title">{strMeal}</h2>
+      <button data-testid="share-btn" type="button">
+        <img src={ shareIcon } alt="share" />
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        <img src={ whiteHeart } alt="favorite" />
+      </button>
+      <h4 data-testid="recipe-category">{strCategory}</h4>
+      <p data-testid="instructions">{strInstructions}</p>
+      <section>
+        <h1>
+          Recomendation
+        </h1>
+        {/* {
+          reco.map((rec, index) => (
+            <div key={ `data-testid="${index}-recomendation-card"` }>
+              <img src={ rec.strDrinkThumb } alt="recomendation" />
+              <h3>{rec.strAlcoholic}</h3>
+              <h3>{rec.strDrink}</h3>
+            </div>
+          ))
+        } */}
+      </section>
+      <button data-testid="start-recipe-btn" type="button">
+        Start Recipe
+      </button>
     </div>
   );
 }
